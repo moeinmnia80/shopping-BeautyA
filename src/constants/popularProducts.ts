b9171422cc542bce26269a535cf7174f1046597f
moein mohammadnia <moeinmohammadnia@yahoo.com>
2024-04-20 15:50:58 +0330

    fix deploy problem

---
+++ src/constants/popularProducts.ts
@@ -0,0 +1,38 @@
+import img1 from "@assets/images/pp1.png";
+import img2 from "@assets/images/pp2.png";
+import img3 from "@assets/images/pp3.png";
+import img4 from "@assets/images/pp4.png";
+
+type PopularProductsType = {
+  id: number;
+  img: string;
+  title: string;
+  text: string;
+};
+
+export const popularProducts: PopularProductsType[] = [
+  {
+    id: 0,
+    img: img1,
+    title: "Beautya Prestige la Mousse Micellaire",
+    text: "Plumping Gloss - Instant and Long-Term Volume Effect - 24h Hydration",
+  },
+  {
+    id: 4,
+    img: img2,
+    title: "Beautya Prestige la Mousse Micellaire",
+    text: "Plumping Gloss - Instant and Long-Term Volume Effect - 24h Hydration",
+  },
+  {
+    id: 2,
+    img: img3,
+    title: "Beautya Prestige la Mousse Micellaire",
+    text: "Plumping Gloss - Instant and Long-Term Volume Effect - 24h Hydration",
+  },
+  {
+    id: 3,
+    img: img4,
+    title: "Beautya Prestige la Mousse Micellaire",
+    text: "Plumping Gloss - Instant and Long-Term Volume Effect - 24h Hydration",
+  },
+];
